237a8e116024dd560859ad85204f66c961be9730
Zachary Beyer <zack@boomlearning.com>
2025-05-24 09:28:14 -0600

    Fixed break in JSON

--- client/components/cards/card templates/monsterHeader.js
+++ client/components/cards/card templates/monsterHeader.js
@@ -1,12 +1,6 @@
-Template.monsterHeader.onCreated(function () {
-	Template.instance.data = Template.instance.data || {
-		name: 'Goblin Scrapper',
-	}
-});
+
 Template.monsterHeader.helpers({
 	name() {
-		const cardName = Template.instance.data.name;
-		console.log('name %o', cardName);
-		return cardName;
+		return 'Goblin Scrapper';
 	}
 });
